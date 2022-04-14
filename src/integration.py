dab2b2f7e35f345e17a599ed816f5c9e940079e9
ecunha1996 <id9411@alunos.uminho.pt>
2022-04-14 19:01:14 +0100

    [ADD] modules for preprocessing and integration

---
+++ src/integration.py
@@ -0,0 +1,4 @@
+
+class Integration:
+    def __init__(self):
+        pass
---
+++ src/preprocessing.py
@@ -0,0 +1,21 @@
+import os
+from bioblend import galaxy
+
+class PreProcessing:
+    def __init__(self, galaxy_instance: galaxy.GalaxyInstance, data_directory: str = os.getcwd()):
+        self.galaxy_instance = galaxy_instance
+        self.data_directory = data_directory
+
+    def upload_data(self, filename):
+        """
+        Method to upload data to galaxy
+        :param filename: name of the file to be uploaded
+        :return:
+        """
+        pass
+
+    def download_data(self):
+        pass
+
+    def generate_report(self):
+        pass
