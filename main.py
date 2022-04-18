9978db2bdc94cc82b8d31f9c3dcbcae6a30bd9f8
ecunha1996 <75447346+ecunha1996@users.noreply.github.com>
2022-04-18 09:13:08 +0100

    [DELETE] main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from bioblend import galaxy
-
-
-def main():
-    gi = galaxy.GalaxyInstance(url='https://usegalaxy.org/', key='b3a37dd4f27915d58f6bbcfe5b9158f2')
-
-
-if __name__ == '__main__':
-    main()
